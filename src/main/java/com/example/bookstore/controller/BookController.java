a22fbca93c90861874e6d5f8a4b94e8e2d71c0b3
KUMAR PATTANAIK <samirpattanaik14@gmail.com>
2022-02-21 13:07:15 +0530

    cross origin access allowed

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.example.bookstore.entity.Book;
 import com.example.bookstore.service.BookService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
 	
 	@Autowired
